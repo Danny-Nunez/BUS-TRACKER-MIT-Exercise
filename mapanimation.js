3a4ebdfb26f16565513db24948310fabe8ce438a
Danny Nunez <114783191+Danny-Nunez@users.noreply.github.com>
2022-12-04 19:06:23 -0500

    Add files via upload

---
+++ mapanimation.js
@@ -0,0 +1,62 @@
+// This array contains the coordinates for all bus stops between MIT and Harvard
+const busStops = [
+  [-71.093729, 42.359244],
+  [-71.094915, 42.360175],
+  [-71.0958, 42.360698],
+  [-71.099558, 42.362953],
+  [-71.103476, 42.365248],
+  [-71.106067, 42.366806],
+  [-71.108717, 42.368355],
+  [-71.110799, 42.369192],
+  [-71.113095, 42.370218],
+  [-71.115476, 42.372085],
+  [-71.117585, 42.373016],
+  [-71.118625, 42.374863],
+];
+
+// TODO: add your own access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXJub2xkLTMyIiwiYSI6ImNrbHY5OGdyNDBiOTMydm52MzIwOHY3cnMifQ.wZ3FxrItEQDpcXcJnO0POA';
+
+// This is the map instance
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  center: [-71.104081, 42.365554],
+  zoom: 13,
+});
+
+// Making gray markers for all of the bus stops
+for(let i = 0; i <busStops.length; i++){
+  var subMarker = new mapboxgl.Marker({
+  color: "#575757"})
+  .setLngLat(busStops[i]).addTo(map);
+}
+
+
+// TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
+var marker = new mapboxgl.Marker({
+  color: "#931b1b"})
+  .setLngLat(busStops[0])
+  .addTo(map);
+
+// counter here represents the index of the current bus stop
+let counter = 0;
+function move() {
+  // TODO: move the marker on the map every 1000ms. Use the function marker.setLngLat() to update the marker coordinates
+  // Use counter to access bus stops in the array busStops
+  if (counter < 11) {
+    counter += 1;
+  }
+  else {
+    counter = 0;
+  }
+  console.log(counter);
+  marker.setLngLat(busStops[counter]);
+  timeout = setTimeout(function(){ move() } , 1000);
+  // Make sure you call move() after you increment the counter.
+}
+
+// Do not edit code past this point
+if (typeof module !== 'undefined') {
+  module.exports = { move };
+}
